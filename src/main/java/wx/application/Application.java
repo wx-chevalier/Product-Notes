da8754412304964c5c0e45a90f1bfbdd24e34c2d
hoteam@live-forest.com <hoteam@live-forest.com>
2016-03-08 23:29:50 +0800

    (1)添加Spring-Boot作为全局依赖于程序入口，替换原来的Maven-Jetty （2）优化全局Spring依赖 （3）完成Maven Profile多环境配置

---
+++ src/main/java/wx/application/Application.java
@@ -0,0 +1,30 @@
+package wx.application;
+
+import com.squareup.okhttp.OkHttpClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.http.HttpClient;
+
+/**
+ * Created by apple on 16/3/8.
+ */
+@RestController
+@EnableAutoConfiguration
+@ImportResource("classpath:spring/applicationContext.xml")
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
