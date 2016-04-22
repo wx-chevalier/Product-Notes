8f81cb7c7cce6fe4986a9aa12caafd80c8736786
wxyyxc1992 <hoteam@live-forest.com>
2016-04-22 16:35:54 +0800

    添加测试依赖与管理依赖

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+- [RARF-Java](#rarf-java)
+  - [Features](#features)
+- [Run](#run)
+  - [Maven](#maven)
+  - [Gradle](#gradle)
+
 # RARF-Java
 Implementation of Reactive Abstract Resource Flow Architecture Style In Java With Spring
 
@@ -7,13 +13,18 @@ Implementation of Reactive Abstract Resource Flow Architecture Style In Java Wit
 # Run
 ## Maven
 - mvn install : 安装基本的Maven依赖项
+
 - mvn spring-boot:run : 从wx.application.Application开始运行服务器
+
 - mvn package -Ppro : 按照开发环境打包成jar文件
+
 - mvn package -Pdeploy : 按照部署环境打包成war文件
 
 ## Gradle
 - gradle bootRun:运行开发环境
+
 - gradle prod bootRun:运行生产环境
+
 - gradle deploy bootRun:运行部署环境
 
-- gradle rePackage
+- gradle bootRepackage:打包成Jar包可以独立运行/放置在Jetty下运行
--- src/main/java/wx/application/Application.java
+++ src/main/java/wx/application/Application.java
@@ -14,7 +14,10 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
 import wx.webservice.hessian.server.HessianConfiguration;
+
+import java.util.Arrays;
 
 /**
  * Created by apple on 16/3/8.
@@ -25,7 +28,15 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
 
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
 
     }
 
